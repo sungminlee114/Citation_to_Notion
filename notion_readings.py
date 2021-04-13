ba2bb7505d58e2283d6c79d61dceea8913479da1
SungminLee <starmin114@yonsei.ac.kr>
2021-04-13 23:19:22 +0900

    Checkpoint for adding similarity feature

--- notion_readings.py
+++ notion_readings.py
@@ -1,7 +1,10 @@
+import enum
 from notion.client import NotionClient
 import requests
 from bs4 import BeautifulSoup
 import re
+from difflib import SequenceMatcher
+import time
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,9 +18,9 @@ class bcolors:
     UNDERLINE = '\033[4m'
     WHITE = '\033[97m'
 
-NOTION_TOKEN_V2 = "<notion_v2_key>"
-NOTION_COLLECTION_URL = "<notion_v2_url>"
-GOOGLE_COOKIE = "<google_scholar_cookie>"
+NOTION_TOKEN_V2 = "37b60a58b9743c86038024906c7b4a8a4803cefeff6d014fe9ed4bd07df8b0d32f469f718c87c5c7275b4281abbc06445eaa9d3c4c4f1a6fb61f1110ecc187df445cc3ee5b65517a04abb71b0015"
+NOTION_COLLECTION_URL = "https://www.notion.so/5f2d5da3b84a4a5789049ae13396d656?v=b2d9985560e94c4c8266c223fcb844a3"
+GOOGLE_COOKIE = "ANID=AHWqTUnVSXCqrlNknCPeuYPNUAtTjNqcAKJxWeVMMRhNzYBBQHQJA9kX8FSXkMN-; SEARCH_SAMESITE=CgQIm5IB; SID=8gd7IbXDG7-zO4L8Ya9JTqLW6vR1_XczhVe6zzh2I10bar0WasnGEH41HPh80hA1DrsAeA.; __Secure-3PSID=8gd7IbXDG7-zO4L8Ya9JTqLW6vR1_XczhVe6zzh2I10bar0WaECeEIHgZj7NpWYJbVbkgQ.; HSID=AdGdUc_IAYJQoBM4g; SSID=AcTcsthP374_d8iYi; APISID=VgsqG-CnQLMZt0cq/AX6AQNr4LsNjQZf8w; SAPISID=zX8i2TOmWjlCvvkh/AyrzkN5l54BnkIzgq; __Secure-3PAPISID=zX8i2TOmWjlCvvkh/AyrzkN5l54BnkIzgq; SIDCC=AJi4QfGZZ60Izo-eqAKf-18ClI-fkdiARhkjDVEOEpcePvITPQIltqHA0AmUf5ntiy5h_2SHq6M; __Secure-3PSIDCC=AJi4QfGSKoXPHFBMlZhrRX3jKlMsTaUBMBCSVGPNmCNwEGgxoTuH7KpxCdO8w41cyJ3cebB7GA; NID=213=klPRUP3s2DqG-LAu1UW_07WGAnbONe8V-2RIBojH5rSBA4_-62LX8cu9n7N41FFHjDkNRCxhBywlYFY4VBI3BF2OFIbOsg7FgFwwttbbTsaLXGa3SA-euRUGSVWY2czS_Lp2mehgJ4iqaGf-mO7s6d9X9AzLw8Bd_JJXGvq-oD5dBS4k03GymnLlj4KGi0eKEGfT4_yCuMMXstOcO4bjFQuTE_-k4ZIsYr11vK-iwhdnoj0aCUMKVOMiIH_HBLHHbpcF8w; 1P_JAR=2021-04-13-12; GSP=A=z3XPkQ:CPTS=1618319982:LM=1618319982:S=_XhTdSaneSlNbzkG"
 
 client = NotionClient(token_v2=NOTION_TOKEN_V2)
 cv = client.get_collection_view(NOTION_COLLECTION_URL).collection
@@ -36,20 +39,31 @@ ref_str = ref_str.replace("\n", " ")
 
 URL = "https://scholar.google.com/scholar?hl=ko&as_sdt=0%2C5&q="
 
-summary_exsisting = 0
 summary_no_result = 0
-summary_many_no_matching = 0
 summary_total_queried = 0
 
 for query in ref_str.split("["):
     query = query[query.find("]")+1:]
-    if "http" in query and "//" in query:
-        continue
+    
+    #Get rid of urls
+    query = re.sub(",?\s?h\\s?t\\s?t\\s?p\\s?s?\\s?:\\s?\/\\s?\/\\s?[-a-zA-Z0-9@:%._\+~#=\\s]{1,256}\.\s?[a-zA-Z0-9()\s]{1,6}\s?([-a-zA-Z0-9()@:%_\+.~#?&//=\s]*),?\s?", "", query)
     if query:
         summary_total_queried += 1
         print(f"---[{summary_total_queried} : {len(query)}]---")
         
-        querys =[q.strip() for q in query.split(".")]
+        dot_1 = re.search("([A-Z]((.+?\.(?= \d{4}))|(.+?and .+?\.(?= [a-zA-Z].*))|(.+?et .+?\.(?= [a-zA-Z].*))))", query)
+        querys = []
+        if dot_1 is not None:
+            dot_1 = dot_1.end()
+            part_1 = query[:dot_1 - 1].strip()
+            querys = [part_1] + [q.strip() for q in re.split("\.(?!,)", query[dot_1:])]
+        else:
+            querys = [q.strip() for q in re.split("\.(?!,)", query)]
+        
+        querys = [q for q in querys if q != ""]
+        print(querys)
+        # continue
+
         query_str = ""
         l = 0
         for i, q in enumerate(querys):
@@ -59,32 +73,42 @@ for query in ref_str.split("["):
             l += len(q)
             if(l < 250):
                 query_str += q
-                
-        print(f"Querying [{query_str}]...\n")
-        queryURL = URL + query_str + "&btnG="
-
-        response = requests.get(queryURL, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36', 'cookie' : GOOGLE_COOKIE})
         
-        soup = BeautifulSoup(response.text, 'html.parser')
-        results = soup.find(id="gs_res_ccl_mid")
+        query_strs = [query_str] + querys
 
-        if not response.ok or results is None:
-            print(f"{bcolors.FAIL}Connection Failed.. {bcolors.WHITE}")
-            if response.status_code == 429:
-                print(response.headers)
-                print(response.text)
-                exit()
-            continue
-        else:
-            if len(results) == 0:
-                print(f"{bcolors.WARNING}No results..{bcolors.WHITE}")
-                summary_no_result += 1
+        title_similar = 0
+        result = None
+        title_el = None
+
+        for query_str in query_strs:
+                
+            print(f"Querying [{query_str}]...\n")
+            queryURL = URL + query_str + "&btnG="
+
+            # querys = [q.lower() for q in querys]
+
+            response = requests.get(queryURL, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36', 'cookie' : GOOGLE_COOKIE})
+            
+            soup = BeautifulSoup(response.text, 'html.parser')
+            results = soup.find(id="gs_res_ccl_mid")
+
+            if not response.ok or results is None:
+                print(f"{bcolors.FAIL}Connection Failed.. {bcolors.WHITE}")
+                if response.status_code == 429:
+                    print(response.headers)
+                    print(response.text)
+                    exit()
                 continue
+            # else:
+            #     if len(results) == 0:
+            #         print(f"{bcolors.WARNING}No results..{bcolors.WHITE}")
+            #         summary_no_result += 1
+            #         continue
 
             result_inner = results.find_all(class_ = "gs_ri")
 
-            manyFlag = True if len(result_inner) > 1 else False
-            manyCnt = 0
+            # manyFlag = True if len(result_inner) > 1 else False
+            # manyCnt = 0
             print(f"Total {len(result_inner)} results found.")
 
             # if manyFlag:
@@ -94,95 +118,170 @@ for query in ref_str.split("["):
 
             for result in result_inner:
                 title_el = result.find(class_="gs_rt").find("a")
-                title = title_el.get_text()
 
-                if manyFlag:
-                    if title not in querys:
-                        continue
-                    else:
-                        manyCnt += 1
-
-                search_res = cv.get_rows(search=title)
-                if len(search_res) > 0 and search_res[0].name == title:
-                    summary_exsisting += 1
-                    print("Paper already is in table")
+                if title_el is None:
                     continue
 
-                link = title_el['href']
-                
-                author_str = result.find(class_ = "gs_a").get_text()
-                author_str = author_str.replace("\xa0", " ")
+                title = title_el.get_text()
 
-                authors, journal, _ = author_str.split(" - ")
-                year = re.findall('\d{4}', journal)
-                if len(year) == 0:
-                    year = -1
-                else:
-                    year = year[0]
+                for q in querys:
+                    # print(title, ";", q.lower(), SequenceMatcher(None, title.lower(), q.lower()).ratio())
+                    if title is q:
+                        title_similar = 1
+                    else :
+                        title_similar = max(SequenceMatcher(None, title.lower(), q.lower()).ratio(), title_similar)
 
-                _journal = journal[:journal.rfind(year)].rstrip().lstrip().replace(",", "")
-
-                if not _journal:
-                    journal = "Not specified"
-                else:
-                    journal = _journal
-                
-                authors2 = authors.replace("…,", "").replace("…", "")
-                authors2 = authors2[:authors2.find(",")].split(" ")
-                authors2 = [auth for auth in authors2 if len(auth)>1][0]
-
-                journal2 = journal.replace("…,", "").replace("…", "").rstrip().lstrip()
-                
-                flag_j = False
-                flag_a = False
-                for el in query.split("."):
-                    if authors2 in el:
-                        authors = el
-                        flag_a = True
-                        continue
-                    if journal2 in el:
-                        journal = el
-                        flag_j = True
-                        continue
-                
-                if not flag_a:
-                    print(f"{bcolors.OKBLUE}Authors not matched.[{authors2}]{bcolors.WHITE}")
-                
-                if not flag_j:
-                    print(f"{bcolors.OKBLUE}Journals not matched.[{journal2}]{bcolors.WHITE}")
-
-                citation_str = result.find(class_="gs_fl")
-                citation_num = re.findall('\d*회', citation_str.get_text())
-                if len(citation_num) == 0:
-                    citation_num = 0
-                else:
-                    citation_num = citation_num[0][:-1]
-
-                print(f"title : {title}")
-                print(f"link : {link}")
-                print(f"authors : {authors}")
-                print(f"journal : {journal}")
-                print(f"year : {year}")
-                print(f"citation : {citation_num}")
-                print()
-
-                year = int(year)
-                citation_num = int(citation_num)
-                
-                row = cv.add_row()
-                row.name = title
-                row.link = link
-                row.authors = authors
-                row.year = year
-                row.conference_journal = journal
-                row.citation = citation_num
-
-                if manyCnt > 0:
+                if title_similar >= 0.7:
                     break
+
+            if title_similar >= 0.7:
+                break
+
+                # if result_index == len(result_inner) -1 and title_similar <= 0.5:
+                #     #kind of recursive; find until title matches
+                #     for q in querys:
+                #         query_str = q
+                                
+                #         print(f"Querying [{query_str}]...\n")
+                #         queryURL = URL + query_str + "&btnG="
+
+                #         # querys = [q.lower() for q in querys]
+
+                #         response = requests.get(queryURL, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36', 'cookie' : GOOGLE_COOKIE})
+                        
+                #         soup = BeautifulSoup(response.text, 'html.parser')
+                #         result = soup.find(id="gs_res_ccl_mid")
+
+                #         if not response.ok or result is None:
+                #             print(f"{bcolors.FAIL}Connection Failed.. {bcolors.WHITE}")
+                #             if response.status_code == 429:
+                #                 print(response.headers)
+                #                 print(response.text)
+                #                 exit()
+                #             continue
+                #         else:
+                #             if len(result) == 0:
+                #                 continue
+
+                #         result_inner = result.find_all(class_ = "gs_ri")
+                #         result = result_inner[0]
+                #         title_el = title_el.find(class_="gs_rt").find("a")
+                    
+                #         if _title_el is None:
+                #             continue
+
+                #         title = title_el.get_text()
+
+                #         title_similar = max(SequenceMatcher(None, _title.lower(), q.lower()).ratio(), title_similar)
+
+                #         if(title_similar > 0.7):
+                #             break
+
+                # if manyFlag:
+                #     if title_similar > 0.5:
+                #         manyCnt += 1
+
+                #     if manyCnt == 0:
+                #         continue
+
+        if title_similar < 0.7 or title_el is None or result is None:
+            print(f"{bcolors.WARNING}No results..{bcolors.WHITE}")
+            summary_no_result += 1
+            break
+
+        print(f"Title similarity : {title_similar}")
+        
+        # search_res = cv.get_rows(search=title)
+        # if len(search_res) > 0 and search_res[0].name == title:
+        #     summary_exsisting += 1
+        #     print("Paper already is in table")
+        #     continue
+
+        link = title_el['href']
+        
+        author_str = result.find(class_ = "gs_a").get_text()
+        author_str = author_str.replace("\xa0", " ").split(" - ")
+
+        if len(author_str) == 3:
+            authors, journal, _ = author_str
+        elif len(author_str) == 2:
+            authors, _ = author_str
+        else:
+            print(f"{bcolors.WARNING}Author info error. [{author_str}]{bcolors.WHITE}")
+        
+        year = re.findall('\d{4}', journal)
+        if len(year) == 0:
+            year = -1
+        else:
+            year = year[0]
+
+        _journal = journal[:journal.rfind(year)].rstrip().lstrip().replace(",", "")
+
+        if not _journal:
+            journal = "Not specified"
+        else:
+            journal = _journal
+        
+        #from google
+        # authors2 = authors.replace("…,", "").replace("…", "")
+        # authors2 = authors2.replace(",", " ").replace("  ", " ").strip().lower()
+
+        journal2 = journal.replace("…,", "").replace("…", "").rstrip().lstrip().lower()
+        
+        # flag_a = False
+        flag_j = False
+        for el in querys:
+            _el = el
+            el = el.lower()
+            # print(authors2, ";", el.replace(",", " ").replace(".", " ").replace("  ", " "), SequenceMatcher(None, authors2, el.replace(",", " ").replace(".", " ").replace("  ", " ")).ratio())
+            # if authors2 in el or SequenceMatcher(None, authors2, el.replace(",", " ").replace(".", " ").replace("  ", " ")).ratio() > 0.5:
+            #     authors = _el
+            #     flag_a = True
+            #     continue
             
-            if manyCnt == 0 and manyFlag:
-                print(f"{bcolors.WARNING}Many results found, but none of the results matched.{bcolors.WHITE}")
-                summary_many_no_matching += 1
+            # print(journal2, ";", el)
+            if journal2 in el or SequenceMatcher(None, journal2, el).ratio() > 0.5:
+                journal = _el
+                flag_j = True
+                continue
+        
+        # if not flag_a:
+        #     print(f"{bcolors.OKBLUE}Authors not matched.[{authors2}]{bcolors.WHITE}")
+        
+        if not flag_j:
+            print(f"{bcolors.OKBLUE}Journals not matched.[{journal2}]{bcolors.WHITE}")
+
+        citation_str = result.find(class_="gs_fl")
+        citation_num = re.findall('\d*회', citation_str.get_text())
+        if len(citation_num) == 0:
+            citation_num = 0
+        else:
+            citation_num = citation_num[0][:-1]
+
+        print(f"title : {title}")
+        print(f"link : {link}")
+        print(f"authors : {authors}")
+        print(f"journal : {journal}")
+        print(f"year : {year}")
+        print(f"citation : {citation_num}")
+        print()
+
+        year = int(year)
+        citation_num = int(citation_num)
+        
+        # row = cv.add_row()
+        # row.name = title
+        # row.link = link
+        # row.authors = authors
+        # row.year = year
+        # row.conference_journal = journal
+        # row.citation = citation_num
+
+        time.sleep(1)
+        
+        # if manyCnt == 0 and manyFlag:
+        #     print(f"{bcolors.WARNING}Many results found, but none of the results matched.{bcolors.WHITE}")
+        #     summary_many_no_matching += 1
 
 
-print(f"{bcolors.OKGREEN}[Summary]\nOut of {summary_total_queried} of querys,\nExsisting : {summary_exsisting}\nNo result : {summary_no_result}\n Many but no matching result : {summary_many_no_matching}\n{bcolors.WHITE}")
+print(f"{bcolors.OKGREEN}[Summary]\nOut of {summary_total_queried} querys,\nNo result : {summary_no_result}\n{bcolors.WHITE}")
